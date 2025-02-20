5687c53e0635f6738bac00122c555f657cf1be7d
Ayan Jhunjhunwala <ayanqwerty564@gmail.com>
2025-02-19 22:50:14 -0800

    files

---
+++ guitar-chords.v1i.folder/README.dataset.txt
@@ -0,0 +1,6 @@
+# guitar-chords > 2024-07-23 6:44pm
+https://universe.roboflow.com/hlcv-jtcas/guitar-chords-eu59l
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not open video stream")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Failed to capture frame")
+        break
+
+    cv2.imshow('Live Webcam', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
